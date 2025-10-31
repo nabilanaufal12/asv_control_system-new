8fdb15e370a9973116a3a9ec1c4ac1e74750d822
nabilanaufal12 <2401020073@student.umrah.ac.id>
2025-11-01 04:04:05 +0700

    Subuh debug

--- src/navantara_web/js/main_local.js
+++ src/navantara_web/js/main_local.js
@@ -2,7 +2,7 @@
 
 // === MODIFIKASI: Definisikan IP Server di satu tempat ===
 // IP 192.168.1.18 diambil dari config.json Anda.
-const SERVER_IP = "http://192.168.2.174:5000";
+const SERVER_IP = "http://192.168.1.18:5000";
 
 // Variabel Global untuk Peta Leaflet
 let map;
